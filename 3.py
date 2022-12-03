5c4c71e430a5b8d598401376eef1b4c70abd8435
cytocracy <theop.parker@gmail.com>
2022-12-02 21:11:41 -0800

    3

---
+++ 3.py
@@ -0,0 +1,34 @@
+with open('input/3.txt') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+values = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26, "A": 27, "B": 28, "C": 29, "D": 30, "E": 31, "F": 32, "G": 33, "H": 34, "I": 35, "J": 36, "K": 37, "L": 38, "M": 39, "N": 40, "O": 41, "P": 42, "Q": 43, "R": 44, "S": 45, "T": 46, "U": 47, "V": 48, "W": 49, "X": 50, "Y": 51, "Z": 52}
+
+sum = 0
+'''
+Part 1
+'''
+# for line in content:
+#     first = line[0: len(line)//2]
+#     second = line[len(line)//2: len(line)]
+
+#     for char in first:
+#         if char in second:
+#             sum += values[char]
+#             break
+
+'''
+Part 2
+'''
+for i in range(0, len(content), 3):
+    first = content[i]
+    second = content[i+1]
+    third = content[i+2]
+
+    for char in first:
+        if char in second and char in third:
+            sum += values[char]
+            break
+
+print (sum)
+    
