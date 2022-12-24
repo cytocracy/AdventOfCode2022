9d3f008d85fa550e0392d3d0610b2116ac7d272e
cytocracy <theop.parker@gmail.com>
2022-12-24 07:33:11 -0800

    23 done

--- 23.py
+++ 23.py
@@ -58,11 +58,12 @@ def get_west_neighbors(r, c):
     return neighbors
 
 def get_bounds(elves):
-    print(len(elves))
+    # print(len(elves))
     minx = min([x[0] for x in elves])
     maxx = max([x[0] for x in elves])
     miny = min([x[1] for x in elves])
     maxy = max([x[1] for x in elves])
+    print(minx, maxx, miny, maxy)
     return minx, maxx, miny, maxy
 
 def get_num_empty(elves, bounds):
@@ -76,7 +77,17 @@ def get_num_empty(elves, bounds):
 
 proposed = {}
 
-def print(elves):
+# def print(elves):
+#     minx, maxx, miny, maxy = get_bounds(elves)
+#     for r in range(minx, maxx+1):
+#         for c in range(miny, maxy+1):
+#             if (r,c) in elves:
+#                 print('#', end='')
+#             else:
+#                 print('.', end='')
+#         print()
+
+def print_elves(elves):
     minx, maxx, miny, maxy = get_bounds(elves)
     for r in range(minx, maxx+1):
         for c in range(miny, maxy+1):
@@ -86,55 +97,76 @@ def print(elves):
                 print('.', end='')
         print()
 
+
 def run(elves):
 
-
-    for i in range(10):
+    for i in range(1000):
+        proposed = {}
+        # print(directions)
         
         newcoords = set()
-        print(i)
+        print(i+1)
         for elf in elves:
             neighbors = get_neighbors(*elf)
-            if not any([n in elves for n in neighbors]):
-                newcoords.add(elf)
-                continue
-            for direction in directions:
-                if direction == (1,0):
-                    neighbors = get_south_neighbors(*elf)
-                    if not any([n in elves for n in neighbors]):
-                        proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
-                        break
-                elif direction == (-1,0):
-                    neighbors = get_north_neighbors(*elf)
-                    if not any([n in elves for n in neighbors]):
-                        proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
-                        break
-                elif direction == (0,1):
-                    neighbors = get_east_neighbors(*elf)
-                    if not any([n in elves for n in neighbors]):
-                        proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
-                        break
-                elif direction == (0,-1):
-                    neighbors = get_west_neighbors(*elf)
-                    if not any([n in elves for n in neighbors]):
-                        proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
-                        break
-
+            if any([n in elves for n in neighbors]):
+                # newcoords.add(elf)
+                
+                for i in range(4):
+                    direction = directions[i]
+                    if direction == (1,0):
+                        neighbors = get_south_neighbors(*elf)
+                        if not any([n in elves for n in neighbors]):
+                            proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
+                            break
+                    elif direction == (-1,0):
+                        neighbors = get_north_neighbors(*elf)
+                        if not any([n in elves for n in neighbors]):
+                            proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
+                            break
+                    elif direction == (0,1):
+                        neighbors = get_east_neighbors(*elf)
+                        if not any([n in elves for n in neighbors]):
+                            # if elf == (0,1):
+                                # print('east')
+                            proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
+                            break
+                    elif direction == (0,-1):
+                        neighbors = get_west_neighbors(*elf)
+                        if not any([n in elves for n in neighbors]):
+                            proposed[elf] = (elf[0]+direction[0], elf[1]+direction[1])
+                            break
+            # if elf == (0,1):
+            #     print(elf, proposed[elf])
+            
         # newcoords = set()
-        for elf in proposed:
-            newloc = proposed[elf]
-            if len([x for x in proposed if proposed[x] == newloc]) == 1:
-                newcoords.add(newloc)
-            else:
-                newcoords.add(elf)
+        moved = False
+        for elf in elves:
+            newloc = elf
+            if elf in proposed:
+                # print('proposed', elf, proposed[elf])
+                length = len([x for x in proposed if proposed[x] == proposed[elf]])
+                # print(length)
+                if length == 1:
+                    # print('yay')
+                    moved = True
+                    newloc = proposed[elf]
+                
+            newcoords.add(newloc)
+        if not moved:
+            print(i+1)
+            break
+        # i += 1
+
         elves = newcoords
         directions.append(directions.pop(0))
+        # print(len(elves))
+        # print_elves(elves)
 
+    return elves
 
-run(elves)
-
-
-
+# print(len(elves))
+elves = run(elves)
+# print(len(elves))
 print(get_num_empty(elves, get_bounds(elves)))
         
 
